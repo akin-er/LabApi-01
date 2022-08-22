bb3dfcc353848a291bd0465319d1d653170a1412
Akın ER <akin.er@nswta.de>
2022-08-22 06:28:45 +0300

    test vers incr.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-var version = "0000A007"; 
+var version = "0000A008"; 
 
 app.MapGet("/", () => $"NSWTA TEST API APP! {version}-{Guid.NewGuid()}");
 
