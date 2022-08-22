d905b9365d25cb2993dc63d3ddffef81aae76560
Akın ER <akin.er@nswta.de>
2022-08-22 05:22:17 +0300

    ci cd test

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-var version = "0000A003"; 
+var version = "0000A004"; 
 
 app.MapGet("/", () => $"NSWTA TEST API APP! {version}");
 
